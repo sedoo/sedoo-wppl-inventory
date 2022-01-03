995143295ffc62dde668c575632836e0b6868fdb
GregCass <11537642+GregCass@users.noreply.github.com>
2022-01-03 17:05:54 +0100

    First Commit

---
+++ sedoo-wppl-inventory.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:     Sedoo Inventory
+ * Plugin URI:      https://github.com/sedoo/sedoo-wppl-inventory
+ * Description:     Infos détaillées sur l'ensemble des sites internet gérées par le SEDOO
+ * Author:          Pierre VERT , Grégoire CASSAGNEAU - SEDOO DATA CENTER
+ * Author URI:      https://www.sedoo.fr 
+ * Text Domain:     sedoo-wppl-inventory
+ * Domain Path:     /languages
+ * Version:         0.0.1
+ * GitHub Plugin URI: sedoo/sedoo-wppl-inventory
+ * GitHub Branch:     master
+ * @package         Sedoo_Wppl_Inventory
+ */
