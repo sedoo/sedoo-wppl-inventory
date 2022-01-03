e2a06a451ad597204320eb91588dc05c4de2f82b
GregCass <11537642+GregCass@users.noreply.github.com>
2022-01-03 18:22:34 +0100

    Structure du plugin

--- sedoo-wppl-inventory.php
+++ sedoo-wppl-inventory.php
@@ -12,3 +12,20 @@
  * GitHub Branch:     master
  * @package         Sedoo_Wppl_Inventory
  */
+
+// LOAD CSS & SCRIPTS 
+function sedoo_wppl_inventory_scripts() {
+    wp_register_style( 'prefix-style', plugins_url('css/sedoo_inventory.css', __FILE__) );
+    wp_enqueue_style( 'prefix-style' );
+}
+ add_action('wp_enqueue_scripts','sedoo_wppl_inventory_scripts');
+
+//  INCLUDE REGISTER POST TYPE AND TAXONOMIES FUNCTIONS
+// CPT - POST TYPES
+include 'post-types/sedoo-wppl-inventory-application.php';
+include 'post-types/sedoo-wppl-inventory-contact.php';
+// TAXONOMIES
+include 'taxonomies/sedoo-wppl-inventrory-instance-wp.php';
+include 'taxonomies/sedoo-wppl-inventrory-type-de-site.php';
+include 'taxonomies/sedoo-wppl-inventrory-structure.php';
+include 'taxonomies/sedoo-wppl-inventrory-server.php';
