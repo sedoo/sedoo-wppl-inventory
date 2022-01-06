d1a40db9c9f42b9a7b90729935f327deae152d14
GregCass <11537642+GregCass@users.noreply.github.com>
2022-01-06 17:42:25 +0100

    Changing Front Display of Single Jeudi 6/01/2022 - 17h42

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -25,18 +25,31 @@ $image_app = get_field('sedoo_inventory_image_app');
             <main id="main" class="site-main">
                 <article id="post-<?php the_ID();?>">	
                     <header>
-						<h3><b>URL: </b><?php echo $url_app; ?></h3>
-						<h3><b>BACKOFFICE: </b><?php echo $url_backoff; ?></h3>
-						<span>  <?php echo $url_app; ?></span>
-						<div>
-							
-							<div>
-								<a title="<?php echo __( 'Project website', 'sedoo-wppl-projects' ); ?>" target="_blank" href=" <?php echo $url_backoff; ?>"> <?php echo $url_backoff; ?> </a>
-							</div>
-						</div>
+						
+						<h1><?php the_title(); ?></h1>
+					
+						<?php if($url_app) { ?>
+						<p class="infoApptext"><b>URL: </b><span class="colorGrey"><a href="<?php echo $url_app; ?>" title="lien vers <?php echo $url_app; ?> "><?php echo $url_app; ?></a></span></p>
+						<?php } ?>
+						
+						<?php if($url_backoff) { ?>
+						<p class="infoApptext"><b>BACKOFFICE: </b><span class="colorGrey"><a href="<?php echo $url_backoff; ?>" title="lien vers <?php echo $url_backoff; ?>" ><?php echo $url_backoff; ?></a></span></p>
+						<?php } ?>
+						
+						<?php if($contact_app) { ?>
+						<p class="infoApptext"><b>CONTACT: </b><span class="colorGrey"><?php echo $contact_app; ?></span></p>
+						<?php } ?>
+
+						<?php if($date_app) { ?>
+						<p class="infoApptext"><b>DATE: </b><span class="colorGrey"><?php echo $date_app; ?></span></p>
+						<?php } ?>
+						
+						<?php if($password_app) { ?>
+						<p class="infoApptext"><b>MOT DE PASSE: </b><span class="colorGrey"><?php echo $password_app; ?></span></p>
+						<?php } ?>
 
 						<?php if($image_app) { ?>
-							<figure>
+							<figure class="imgApp">
 								<img src="<?php echo $image_app; ?>">
 							</figure>
 						<?php } ?>
@@ -44,12 +57,11 @@ $image_app = get_field('sedoo_inventory_image_app');
                     <section>
                         <?php the_content(); ?>
                     </section>
-                    <?php if (get_field("sources")){ ?>
-                    <footer class="sources">
-                        <h2><?php echo __('Sources', 'sedoo-wpth-labs'); ?> :</h2>
-                        <p><?php the_field("sources") ?></p>
+              
+                    <footer class="">
+                        
                     </footer>
-                    <?php } ?>
+                 
                
 				</article>
             
@@ -60,70 +72,87 @@ $image_app = get_field('sedoo_inventory_image_app');
 					
 					</section>
 					<?php 
-						if($contact_app || $ldap_connect) {
-							echo '<section class="sedoo-project-section-urls">';
-							echo '<h2> URL </h2>';
-						}
-						if($contact_app) {
-							echo '<a href="'.$contact_app.'">'.__( 'Data access', 'sedoo-wppl-projects' ). '</a>';
-						}
-						if($ldap_connect) {
-							echo '<a href="'.$ldap_connect.'">'.__( 'Official website', 'sedoo-wppl-projects' ). '</a>';	
-						}
+						// if($contact_app || $ldap_connect) {
+						// 	echo '<section class="sedoo-project-section-urls">';
+						// 	echo '<h2> URL </h2>';
+						// }
+						// if($contact_app) {
+						// 	echo '<a href="'.$contact_app.'">'.__( 'Data access', 'sedoo-wppl-projects' ). '</a>';
+						// }
+						// if($ldap_connect) {
+						// 	echo '<a href="'.$ldap_connect.'">'.__( 'Official website', 'sedoo-wppl-projects' ). '</a>';	
+						// }
 
-						if($contact_app || $ldap_connect) {	
-							echo '</section>';
-						}
+						// if($contact_app || $ldap_connect) {	
+						// 	echo '</section>';
+						// }
 					?>
 					<?php 
-						if($thematiques) {
-							echo '<section class="sedoo-project-section-themes">';
-							echo '<h2> '.__( 'Project themes', 'sedoo-wppl-projects' ). ' </h2>';
-							echo '<div class="tag">';
-							foreach($thematiques as $thematique) {
-								echo '<a href="'.get_term_link($thematique->term_id).'">'.esc_html($thematique->name).'</a>';   
-							}
-							echo '</div></section>';
-						}
+						// if($thematiques) {
+						// 	echo '<section class="sedoo-project-section-themes">';
+						// 	echo '<h2> '.__( 'Project themes', 'sedoo-wppl-projects' ). ' </h2>';
+						// 	echo '<div class="tag">';
+						// 	foreach($thematiques as $thematique) {
+						// 		echo '<a href="'.get_term_link($thematique->term_id).'">'.esc_html($thematique->name).'</a>';   
+						// 	}
+						// 	echo '</div></section>';
+						// }
 					?>
 					<?php 
-						if($liste_offres) {
-							echo '<section class="sedoo-project-section-services">';
-							echo '<h2> '.__( 'Project services', 'sedoo-wppl-projects' ). '</h2>';
-							echo '<div class="tag">';
-							foreach($liste_offres as $offre) {
-								echo '<a href="'.get_term_link($offre->term_id).'">'.esc_html($offre->name).'</a>';   
-							}
-							echo '</div></section>';
-						}
+						// if($liste_offres) {
+						// 	echo '<section class="sedoo-project-section-services">';
+						// 	echo '<h2> '.__( 'Project services', 'sedoo-wppl-projects' ). '</h2>';
+						// 	echo '<div class="tag">';
+						// 	foreach($liste_offres as $offre) {
+						// 		echo '<a href="'.get_term_link($offre->term_id).'">'.esc_html($offre->name).'</a>';   
+						// 	}
+						// 	echo '</div></section>';
+						// }
 					?>
 					<?php 
-						if($liste_typologies) {
-							echo '<section class="sedoo-project-section-typologies">';
-							echo '<h2> '.__( 'Project typology', 'sedoo-wppl-projects' ). ' </h2>';
-							echo '<div class="tag">';
-							foreach($liste_typologies as $typlogies) {
-								echo '<a href="'.get_term_link($typlogies->term_id).'">'.esc_html($typlogies->name).'</a>';   
-							}
-							echo '</div></section>';
-						}
+						// if($liste_typologies) {
+						// 	echo '<section class="sedoo-project-section-typologies">';
+						// 	echo '<h2> '.__( 'Project typology', 'sedoo-wppl-projects' ). ' </h2>';
+						// 	echo '<div class="tag">';
+						// 	foreach($liste_typologies as $typlogies) {
+						// 		echo '<a href="'.get_term_link($typlogies->term_id).'">'.esc_html($typlogies->name).'</a>';   
+						// 	}
+						// 	echo '</div></section>';
+						// }
 					?>
 				
 					<?php 
-						$linked_project = get_field('sedoo_projects_projets_en_relation');
-						if($linked_project) {
-							echo '<section class="sedoo-project-section-related-projects">';
-							echo '<h2> '.__( 'Related project', 'sedoo-wppl-projects' ). '</h2>';
-							echo '<ul>';
-							foreach($linked_project as $project) {
-								echo '<li><a href="'.get_permalink($project->ID).'">'.$project->post_title.'</a></li>';
-							}
-							echo '</ul></section>';
-						}
+						// $linked_project = get_field('sedoo_projects_projets_en_relation');
+						// if($linked_project) {
+						// 	echo '<section class="sedoo-project-section-related-projects">';
+						// 	echo '<h2> '.__( 'Related project', 'sedoo-wppl-projects' ). '</h2>';
+						// 	echo '<ul>';
+						// 	foreach($linked_project as $project) {
+						// 		echo '<li><a href="'.get_permalink($project->ID).'">'.$project->post_title.'</a></li>';
+						// 	}
+						// 	echo '</ul></section>';
+						// }
 					?>
 				</aside>
 			<?php
 		?>
 		</div>
+	
 	</div>
+	<style>
+		.colorGrey{
+			color:#999999!important;
+		}
+		.infoApptext{
+			font-size:15px;
+		}
+		.infoApptext a:hover{
+			color:#0000!important!important;
+			text-decoration:none!important;
+		}
+		.imgApp{
+			-webkit-box-shadow: 5px 5px 15px 5px rgba(0,0,0,0.19); 
+box-shadow: 5px 5px 15px 5px rgba(0,0,0,0.19);
+		}
+	</style>
 	
