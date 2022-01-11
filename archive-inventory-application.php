09cb31aad8fe7ca3d788b73c9d22e3648c120576
Greg <gregoirecassagneau@gmail.com>
2022-01-11 17:26:49 +0100

    Mardi 11/01/22 - 17h25

--- archive-inventory-application.php
+++ archive-inventory-application.php
@@ -1,93 +1,55 @@
+<?php if (! defined('ABSPATH')) die('Restricted Area'); ?>
 <?php get_header(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<header class="page-header">
+	<h1 class="container"><?php post_type_archive_title(); ?></h1>
+	</header>
+<?php endwhile; endif; ?>
+<div class="content">
+	<div class="container">
+		<?php
+			$taxonomy = 'structure';
 
-<?php 
+			$term_args = array(
+			 'orderby' => 'name',
+			 'order' => 'ASC'
+			 );
 
+			$terms = get_terms($taxonomy,$term_args);
 
-$app_url = get_field('sedoo_inventory_url_app');
-$app_url_backoff = get_field('sedoo_inventory_url_backoff');
-$app_contact = get_field('sedoo_inventory_contact_app');
-$app_ldap_connect = get_field('sedoo_inventory_ldap_connect');
-$app_date = get_field('sedoo_inventory_date_app');
-$app_password = get_field('sedoo_inventory_password_app');
-$app_image = get_field('sedoo_inventory_image_app');
+			if ($terms) { foreach($terms as $term) {
+		?>
 
-$contact_name = get_field('inventory_contact_name');
-$contact_first_name = get_field('inventory_contact_first_name');
-$contact_mail = get_field('inventory_contact_mail');
-$contact_phone = get_field('inventory_contact_phone');
-$contact_structure = get_field('inventory_contact_structure');
-$contact_img = the_post_thumbnail();
-?>
+		<?php 
+			$args = array(
+			'post_type' => 'sedoo_inventory_app',
+			'tax_query' => array(
+			array(
+				'taxonomy' => 'structure',
+				'terms' => array($term->term_id),
+				'include_children' => true,
+				'operator' => 'IN'
+				)
+			)
+		);
+		$my_query = new WP_Query($args); if ($my_query->have_posts()) { ?>
+			<div class="row">
+				<h1 class="term-title"><?php echo $term->name; ?></h1>
 
-<main class="flexRow"> 
-  
-  <div class="projets flexRow flexWrap padding100 bgColorDarkModeMain">
-  
-    <?php if (have_posts()) : ?>
-  
-      <?php while (have_posts()) : the_post(); ?>
-  
-        <article class="projet flip-card alignCenter w400px margin10 borderSolid3px">
-       
-        <div class="flip-card-inner">
-          <!-- front -->
-          <div class="flip-card-front  alignLeft padding20 borderBox">
-            <?php the_post_thumbnail( 'thumbnail' ); ?>
-    
-            <h1 class="title">
-              <a href="<?php the_permalink(); ?>">
-                <?php the_title(); ?>
-              </a>
-            </h1>
-            <!-- Affichage des Custom Taxonomy -->
+				<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
+					<div class="col-sm-2">
+						<?php if (has_post_thumbnail()) : ?>
+							<?php the_post_thumbnail('thumbnail', array('class' => 'img-responsive aligncenter')); ?>
+						<?php endif; ?>
+						
+						<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e('Permanent link to ', 'textdomain'); ?><?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+					</div>
+				<?php endwhile; ?>
+			</div>
 
-            <!-- Affichage des Custom fields -->
-            <?php if($app_image) { ?>
-            <img class="w100 inlineBlockMid" src="<?php echo $app_image; ?>" />
-            <?php } ?>
-            <?php if($app_url) { ?> 
-            <span class="inlineBlockMid"><b>URL:</b> <?php echo $app_url; ?></span>
-            <?php } ?>
-            <?php if($app_url_backoff) { ?>
-            <span class="inlineBlockMid"><b>BACKOFF:</b> <?php echo $app_url_backoff; ?></span>
-            <?php } ?>
-            <?php if($app_contact) { ?>
-            <span class="inlineBlockMid"><b>CONTACT:</b><?php echo $app_contact; ?></span>
-            <?php } ?>
-            <?php if($app_ldap_connect) { ?>
-            <span class="inlineBlockMid"><b>LDAP:</b><?php if ($app_ldap_connect == 1) : ?> Connecté au LDAP <?php else : ?> Non Connecté au LDAP <?php endif; ?>
-            <?php } ?>
-
-            <!-- resume -->
-            <p><?php the_field('resume'); ?></p>
-            </div>
-          <!-- back -->
-          <div class="flip-card-back  padding20 borderBox"> 
-            <!-- contenu loop  -->
-            <div class="content">
-            <?php echo excerpt(10); ?>
-            </div>
-
-            <div class="btnStand relative top10px bgColorWhite colorBlack"> <a class="bgColorWhite colorBlack" href="<?php the_permalink(); ?>">Voir le projet</a></div>
-          </div>
-        </div>
-        </article> 
-  
-      <?php endwhile; ?>
-    <?php endif; ?>
-  </div><!--
-      --><div class="sidebar w30 sideBarOff">
-  
-          <?php get_sidebar(); ?>
-  
-      </div>
-</main>
-<style>
-  .sideBarOff{
-    display:none;
-  }
-  .sideBarOn{
-    display:block;
-  }
-</style>
+		<?php } 
+		 } } ?>
+		<?php wp_reset_query(); ?>
+	</div>
+</div>
 <?php get_footer(); ?>
--- inc/sedoo-wppl-inventory-functions.php
+++ inc/sedoo-wppl-inventory-functions.php
@@ -52,5 +52,10 @@ if ($post_types) :
 
 endif;
 }
-
+function wpdocs_five_posts_on_homepage( $query ) {
+    if ( $query->is_home() && $query->is_main_query() ) {
+        $query->set( 'posts_per_page', 5 );
+    }
+}
+add_action( 'pre_get_posts', 'wpdocs_five_posts_on_homepage' );
 ?>
